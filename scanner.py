35dd945dcdf8c5d58dca31e6b28744ecfad2cd37
William Harris <w.jharris117@gmail.com>
2026-08-03 20:08:11 -0400

    Added music file scanner

--- scanner.py
+++ scanner.py
@@ -1,1 +1,18 @@
-print("Music Scannner Starting...")
+import os
+def find_music_files(folder):
+    music_files = []
+    for root,directories, files in os.walk(folder):
+        for file in files:
+            if file.lower().endswith(
+                (".mp3", "flac", ".wav", ".m4a")
+            ):
+                full_path = os.path.join(root, file)
+                music_files.append(full_path)
+    return music_files
+
+music_folder = r"C:\Users\wjhar\Music\iTunes"
+
+songs = find_music_files(music_folder)
+
+for song in songs:
+    print(song)
